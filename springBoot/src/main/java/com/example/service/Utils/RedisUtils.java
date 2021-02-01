63b3c8cf796adea148de893ab25815b5623e61ca
codewarrior <codewarrior0330@qq.com>
2021-02-01 16:31:24 +0800

    修改Redis序列化方式，解决Redis可视化工具乱码问题

--- springBoot/src/main/java/com/example/service/Utils/RedisUtils.java
+++ springBoot/src/main/java/com/example/service/Utils/RedisUtils.java
@@ -4,21 +4,39 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 /**
  * redis工具类
  */
+@Component
 public final class RedisUtils {
     Logger logger = LoggerFactory.getLogger(RedisUtils.class);
-    @Autowired
+    @Resource
     private RedisTemplate<String, Object> redisTemplate;
-
+    /**
+     * 修改Redis序列化方式，解决Redis可视化工具乱码问题
+     * @return
+     */
+    @Bean
+    public RedisTemplate redisTemplateInit() {
+        //设置序列化Key的实例化对象
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        //设置序列化Value的实例化对象
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
+    }
     /**
      * 写入缓存
      * @param key
@@ -43,11 +61,46 @@ public final class RedisUtils {
      * @return
      */
     public boolean set(final String key, Object value, Long expireTime){
-        boolean bool = set(key, value);
-        if(bool){
-            return expire(key, expireTime);
+        try{
+            redisTemplate.opsForValue().set(key, value, expireTime);
+            return true;
+        }catch (Exception e){
+            logger.error("redis缓存写入异常:", e);
+            return false;
         }
-        return bool;
+    }
+
+    /**
+     * 写入缓存
+     * @param key
+     * @param value
+     * @return
+     */
+    public boolean setNX(final String key, Object value){
+        try {
+            Boolean aBoolean = redisTemplate.opsForValue().setIfAbsent(key, value);
+            return aBoolean;
+        }catch (Exception e){
+            logger.error("redis缓存写入异常:", e);
+            return false;
+        }
+    }
+
+    /**
+     * 写入缓存同时设置失效时间
+     * @param key
+     * @param value
+     * @param expireTime
+     * @return
+     */
+    public boolean setNX(final String key, Object value, Long expireTime){
+        try{
+            Boolean aBoolean = redisTemplate.opsForValue().setIfAbsent(key, value, expireTime, TimeUnit.SECONDS);
+            return aBoolean;
+        }catch (Exception e){
+            logger.error("redis缓存写入异常:", e);
+            return false;
+        }
     }
 
     /**
@@ -74,11 +127,13 @@ public final class RedisUtils {
      * @return
      */
     public boolean setListMap(final String key, List<Map<String, Object>>listMap, Long expireTime){
-        boolean bool = setListMap(key, listMap);
-        if(bool){
-            return expire(key, expireTime);
+        try{
+            redisTemplate.opsForList().leftPush(key, listMap, expireTime);
+            return true;
+        }catch (Exception e){
+            logger.error("redis写入缓存setForListMap异常：", e);
+            return false;
         }
-        return bool;
     }
 
     /**
@@ -176,6 +231,15 @@ public final class RedisUtils {
     @Nullable
     public List<Map<String, Object>> getListMap(String key){
         return (List<Map<String, Object>>) redisTemplate.opsForList().leftPop(key);
+    }
+    /**
+     * 获取缓存数据,返回List<Map<String, Object>>
+     * @param key
+     * @return
+     */
+    @Nullable
+    public Boolean deleteKey(String key){
+        return redisTemplate.delete(key);
     }
     public Map<Object, Object> getMap(String key){
         return redisTemplate.opsForHash().entries(key);
